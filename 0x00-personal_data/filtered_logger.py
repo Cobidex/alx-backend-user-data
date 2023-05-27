9d37213d40003effbbbb25eacdd6e6dc24b1cfe2
Stephen Obialor <stevobialor@gmail.com>
2023-05-27 11:54:13 +0000

    done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -30,7 +30,7 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
 
     formatter = RedactingFormatter(fields=PII_FIELDS)
-    stream_handler = StreamHandler()
+    stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
 
     logger.addHandler(stream_handler)
