fda94ee3cf4e4c9920f25454a2fd519fdd05201c
Stephen Obialor <stevobialor@gmail.com>
2023-06-01 20:33:36 +0000

    implement the user_id_for_session_id method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,3 +19,11 @@ class SessionAuth(Auth):
         u_id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[u_id] = user_id
         return u_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
