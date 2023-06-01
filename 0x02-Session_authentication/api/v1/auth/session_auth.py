9ec293a4107414a4baa417696eb1713bfe5493bf
Stephen Obialor <stevobialor@gmail.com>
2023-06-01 20:24:40 +0000

    done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,19 @@
 contains the SessionAuth class
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ implements a session based authentication """
-    pass
+    user_id_by_session_id: dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        u_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[u_id] = user_id
+        return u_id
