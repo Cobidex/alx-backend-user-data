01730dfe15507812f38de86ae83be9ac9353ad5a
Stephen Obialor <stevobialor@gmail.com>
2023-06-01 06:03:12 +0000

    create SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+contains the SessionAuth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ implements a session based authentication """
+
