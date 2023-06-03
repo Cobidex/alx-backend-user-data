db66dc08be38a24e47e07881b0291320b90472cf
Stephen Obialor <stevobialor@gmail.com>
2023-06-03 05:49:08 +0000

    add type annotation to make code readable

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -51,5 +51,5 @@ class Auth:
         """
         if request is None:
             return None
-        _my_session_id = getenv('SESSION_NAME')
+        _my_session_id: str = getenv('SESSION_NAME')
         return request.cookies.get(_my_session_id)
